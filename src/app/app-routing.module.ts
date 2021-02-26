1aaaa2d9f149f421dbae87cf1ac1f0a1e309d803
xFran22x <rossigabrielfrancisco2@gmail.com>
2021-02-26 19:50:18 +0100

    Rutas añadidas e implementadas, o eso creo :v

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
+import {SignupComponent} from './signup/signup.component';
+import {VacantsComponent} from './vacants/vacants.component';
+const routes: Routes = [
+  {path: '', pathMatch:'full', redirectTo:'/home'},
+  {path: 'home', component: HomeComponent},
+  {path: 'vacants', component: VacantsComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: '**', redirectTo:'/home'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
