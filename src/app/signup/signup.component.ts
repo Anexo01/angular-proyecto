cb2ccc4367668e00761b84a329cc13f8f410d5a3
xFran22x <rossigabrielfrancisco2@gmail.com>
2021-03-08 18:07:27 +0100

    Sign Up Form

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsuarisService } from '../usuaris.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  public signupForm: FormGroup;
+  email: ''
+  password: ''
+  password2: ''
+  constructor(public usuaris: UsuarisService,private fb: FormBuilder,) { }
 
   ngOnInit(): void {
+    this.crearFormulari();
+  }
+  private crearFormulari(): void {
+    this.signupForm = this.fb.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(8)]],
+        password2: ['', [Validators.required, Validators.minLength(8)]]
+      }
+    );
+  }
+
+  public submit():void{
+    const {email, password} = this.signupForm.value;
+    this.usuaris.validarUsuari(email,password).subscribe((resultat)=>{
+      localStorage.setItem('email',resultat.email);
+      localStorage.setItem('token',resultat.token);
+      this.ngOnInit();
+    })
   }
 
 }
